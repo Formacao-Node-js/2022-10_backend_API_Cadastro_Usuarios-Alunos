6c3f4ad119a84724b8ddd2da3136e4ae4a9e9a7c
Vinícius Soares <vinivsp2304@hotmail.com>
2023-03-27 17:45:19 -0300

    fix jwt controller

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -18,16 +18,16 @@ class TokenController {
       if (!user.passwordIsValid(password)) {
         return res.status(401).json({ response: "Credências inválidas." });
       }
-      const { id, username } = user;
+      const { id, username, role } = user;
       const token = jwt.sign(
-        { id, email, username },
+        { id, email, username, role },
         process.env.TOKEN_SECRET,
         {
           expiresIn: process.env.TOKEN_EXPIRATION,
         }
       );
 
-      return res.status(200).json({ token, user: { id, username, email } });
+      return res.status(200).json({ token, user: { id, username, email, role } });
     } catch (err) {
       return res.status(500).json({ response: "Algo inesperado aconteceu " });
     }
